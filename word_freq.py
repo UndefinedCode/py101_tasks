a31a15c9ce211959d2fb8d05f78b816c3de2b6d5
K TKACHENKO <55198887+UndefinedCode@users.noreply.github.com>
2020-11-08 03:34:57 +0800

    Add last task

--- word_freq.py
+++ word_freq.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """
 Программа считает Топ-100 слов для переданного ей текстого файла.
 
@@ -7,6 +8,51 @@
 
 Тебе может понадобится модуль os, модуль argparse, цикл и словарь
 """
+import nltk
+from nltk.corpus import stopwords
 
 if __name__ == '__main__':
-    pass
+    path = input('Введите путь до файла:')
+    file = open(path, 'r')
+    str = file.read()
+    wordsArr = str.split()
+    result = {}
+
+    for i in range(len(wordsArr)):
+        word = wordsArr[i]
+        last = word[len(word) - 1]
+        splitArg = ['.', ',', '!', '?']
+
+        if last in splitArg:
+            wordsArr[i] = wordsArr[i].replace(last, '')
+
+    for word in wordsArr:
+        stopwordsArr = stopwords.words('russian')
+
+        if word not in stopwordsArr:
+            if word in result:
+                result[word] = result[word] + 1
+            else:
+                result[word] = 1
+
+    topWord = []
+
+    def sort(arr):
+        if len(arr) != 0:
+            top = {'name': '', 'count': 0}
+
+            for word in arr.keys():
+                if result[word] > top['count']:
+                    top = {'name': word, 'count': result[word]}
+
+            del arr[top['name']]
+            topWord.append(top['name'])
+            sort(arr)
+    sort(result)
+
+    index = 0
+    for word in topWord:
+        index += 1
+
+        if index <= 100:
+            print(u'Топ-{} {}'.format(index, word))
